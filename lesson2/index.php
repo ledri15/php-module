c0bcfa4c5e50308f7a4e2c158473f9c67923379a
ledri15 <ledrineziri@gmail.com>
2025-03-13 08:06:35 -0700

    Create index.php

---
+++ lesson2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    
+
+    $x=120;
+    $y=50;
+
+    echo $x + $y;
+    echo $x - $y;
+    echo $x * $y;
+    echo $x / $y;
+    echo $x % $y;
+    
+    $first_string = 'filan fisteku'
+    echo $first_string  'fisteku';
+
+    $second _string = ' fisteku';
+
+
+
+   ?>
+
+</body>
+</html>
