2116a972273b9925d7a7ebf0aa602736f50a841a
ledri15 <ledrineziri@gmail.com>
2025-03-20 07:59:06 -0700

    lesson3

--- lesson3/index.php
+++ lesson3/index.php
@@ -8,50 +8,80 @@
 <body>
     <?php 
     
+    
     $num = -10;
     $age = 18;
     $numri = 23;
     $a = 18;
     $b = 25;
-
-    $age2 = 15
+    $age2 = 15;
 
     if($num > 0){
-     echo "$num is smaller than 0"       
+        echo "$num is greater than 0";
     }
+
+   
+    // if (($age > 18) && ($age < 18)){
+    //     echo "you are a teenager";
+    // } else {
+    //     echo "you are a grown man";
+    // }
+
+    if($numri < 0){
+        echo "The value of $numri is a negative number";
+    } elseif($numri == 0){
+        echo "The value of $numri is 0";
+    } else {
+        echo "The value of $numri is a positive number";
+    }
+
     
-if (($age>18) && ($age<18)){
-    echo "you are a teenager"
-}else{
-    echo " you are a grown man"
-}
+    if($a == $b){
+        echo "1";
+    } elseif ($a > $b){
+        echo "2";
+    } else {
+        echo "3";
+    }
+
     
+    $day = "E Enjte";  
+    switch($day){
+        case "E Hene":
+            echo "Sot eshte dite e Hene";
+            break;
+        case "E Marte":
+            echo "Sot eshte dite e Marte";
+            break;
+        case "E Merkure":
+            echo "Sot eshte dite e Merkure";
+            break;
+        case "E Enjte":
+            echo "Sot eshte dite e Enjte";
+            break;
+        case "E Premte":
+            echo "Sot eshte dite e Premte";
+            break;
+        case "E Shtune":
+            echo "Sot eshte dite e Shtune";
+            break;
+        case "E Diel":
+            echo "Sot eshte dite e Diel";
+            break;
+    }
+
+    $dowhile = 3;
+
+    while ($downwhile > 0) {
+        echo  }
+
+            
+
+$age = array ( "Michel " => 25, "John" => 30, "Peter" => 35 );
+$age = array("Michel" => 25, "John" => 30, ")  
 
 
-if($numri<0){
-    echo "the value of $numri is a negative number"
-}elseif($numri == 0){
-    echo "the value of $numri is 0"
-}else{
-    echo "the value of $numri is a positive number"
-}
 
-
-if( $a == $b ){
-    echo "1";
-}elseif ($a > $b ){
-    echo "2";
-}else{
-    echo "3";
-}
-stwich($age2){
-case (($age2= 0) && ($age2< 18)){
-    echo "you are a minor"}
-case{
-    echo "you are a young adult"{
-    }}
-}
-    
     ?>
 </body>
 </html>
