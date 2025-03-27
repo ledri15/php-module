6a6c412492987bf3f8dd2b47b054c6eb28304ce5
ledri15 <ledrineziri@gmail.com>
2025-03-27 16:18:25 +0100

    Create index.php

---
+++ lesson5/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+$x = 5;
+$y = 10;
+
+function sum(){
+    global $x , $y;
+    $y = $x + $y;
+}
+
+sum();
+echo $y;
+
+?>
+<?php
+
+function Counter(){    
+}
+Counter();
+Counter();
+?>
+<?php
+
+ $Sport = array('Realmadrid','Barcelona','Chelsea','Arsenal');
+echo $Sport = [0]
+
+
+
+?>
+<?php
+
+$Sport = ['Realmadrid','Barcelona','Chelsea','Arsenal'];
+
+
+
+
+?>
