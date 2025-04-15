e69ce41c4b987b1298f3e289e423a8fdafdaa205
ledri15 <ledrineziri@gmail.com>
2025-04-15 16:03:16 +0200

    Create index.php

---
+++ lesson6/index.php
@@ -0,0 +1,46 @@
+<?php
+// $array = array(
+//     array(1,2,3),
+//     array(1,2,3),
+//     array(1,2,3),
+// );
+
+// for ($i=0; $i<4; $i++){
+//     for ($j=0; $j<4; $j++){
+
+//     }
+
+
+// } 
+
+
+
+// for($i=0; $i<6; $i++){
+//     for($j=1; $j<5; $j++){
+//         echo'&nbsp';
+//     }
+//     for($j=1; $j; $j++){
+//         echo'*';
+        
+//     }
+// }
+?>
+
+<?php
+
+
+$grade = array(
+ "math"<= '2',
+ "biology"<= '3',
+ "history"<= '4',
+ "english"<= '5',
+
+)
+
+
+
+
+
+
+
+?>
