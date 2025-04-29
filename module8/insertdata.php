f23d58979133d33b9a3546e6edacf9f8774f9ae1
ledri15 <ledrineziri@gmail.com>
2025-04-29 15:46:00 +0200

    module8

---
+++ module8/insertdata.php
@@ -0,0 +1,53 @@
+ <?php
+
+// try{
+
+// $pdo = new PDO ("mysql:host=localhost;dbname=db" , "root", "");
+
+// $username = "";
+
+// $password = password_hash("mypassword" , PASSWORD_DEFAULT);
+
+// $sql = "INSERT INTO users (username ,password) VALUES ('$username','$password')";
+
+
+// $pdo -> exec($sql);
+// $last_id = $sdo -> lastInsertId();
+// echo "new record created succesflly.",;
+// }
+
+// catch (PODException $e){
+
+//     echo $e -> getMessage();
+
+// }
+
+// try{
+//     $pdo = new PDO ("mysql:host=localhost;dbname=db" , "root", "");
+
+//     $sql = "ALTER TABLE products DROP COLUMN name";
+
+//     $pdo -> exec($sql);
+//     echo "Column droped sucessfully";
+// }
+
+
+// catch (PODException $e){
+
+//     echo $e -> getMessage();
+
+try{
+    $pdo = new PDO ("mysql:host=localhost;dbname=db" , "root", "");
+
+    $sql = "DROP TABLE products";
+
+    $pdo -> exec($sql);
+    echo "Column droped sucessfully";
+}
+
+
+catch(PODException $e){
+
+    echo $e -> getMessage();
+}
+?> 
