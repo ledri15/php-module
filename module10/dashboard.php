007f052a1d6fde96d4f4346ab8d3ff8f5eb41c07
ledri15 <ledrineziri@gmail.com>
2025-05-08 16:14:04 +0200

    module10

--- module10/dashboard.php
+++ module10/dashboard.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        td,th{
+            padding: 10px 20px;
+        }
+
+    </style>
 </head>
 <body>
     <?php
